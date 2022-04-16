06b8f938b1560c8c76a80a3ddd7585867d0e88dc
Hiu Hoang <89955228+HiuHoang@users.noreply.github.com>
2022-04-16 08:43:20 +0700

    init commit

---
+++ admin/connect.php
@@ -0,0 +1,15 @@
+<?php
+
+$dsn = 'mysql:dbname=esmashop;host=3.94.61.123';
+$user = 'hieuhd';
+$pass = '123@123a';
+$option = array(
+	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+);
+
+try {
+	$con = new PDO($dsn, $user, $pass, $option);
+	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	echo 'Failed To Connect' . $e->getMessage();
+}
