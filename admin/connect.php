f46af7e3dfbdb900f50bbd3967888681ed0317a6
Hiu Hoang <89955228+HiuHoang@users.noreply.github.com>
2022-04-16 08:56:44 +0700

    Update Connect.php

--- admin/connect.php
+++ admin/connect.php
@@ -1,15 +1,21 @@
 <?php
 
-$dsn = '3.94.61.123';
-$user = 'hieuhd';
-$pass = '123@123a';
-$option = array(
-	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-);
+$servername = '3.94.61.123';
+$username = 'hieuhd';
+$password = '123@123a';
+$database = 'esmashop';
+// $option = array(
+// 	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+// );
 
-try {
-	$con = new PDO($dsn, $user, $pass, $option);
-	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-	echo 'Failed To Connect' . $e->getMessage();
+// try {
+$con = mysqli_connect($servername, $username, $password, $database);
+if (!$con) {
+	echo "Connection Fail";
+} else {
+	echo "Connection Success";
 }
+	//$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+// 	echo 'Failed To Connect' . $e->getMessage();
+// }
